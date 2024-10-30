7a783af97d7fe18b0c4a0c7a9a9650cc9e317753
Dewa Wono Ichsan <dewa.wono.ichsan@gmail.com>
2024-10-30 22:43:39 +0700

    Add object constructor todo list
    Create object contructor for store property of
    todo list from user. Property of to do list
    include title, description, dueDate, priority,
    and checklist.
    
    Export function object constructor from
    data-todo-list.js file to index.js file.
    Test object constructor to dolist with
    console browser.

---
+++ src/data-todo-list.js
@@ -0,0 +1,10 @@
+function list(title, description, dueDate, priority, notes, checklist){
+    this.title = title
+    this.description = description
+    this.dueDate = dueDate
+    this.priority = priority
+    this.notes = notes
+    this.checklist = checklist
+}
+
+export default list
--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './styles.css'
+import toDoListData from './data-todo-list.js'
+
+const jogging = new toDoListData(
+    'jogging',
+    'run from Jakarta to Tangerang',
+    '1-9-2024',
+    'high',
+    'get water and bread for journey',
+)
+console.warn(jogging);
