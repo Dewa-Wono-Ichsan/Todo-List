fde09cf67b25c6f8876d149536e52f4ad35363e5
Dewa Wono Ichsan <dewa.wono.ichsan@gmail.com>
2024-11-07 07:15:26 +0700

    Create function addList for store user's todo list data
    Create parameter title, description, dueDate,
    priority, notes, and checklist in function
    addList. Use these parameter for create object
    with object constructor list.
    
    Change declaration method myList variable with
    type data array from let to const. Use const
    declaration method because myList variable need
    to stay as array type.
    
    Export function addList from data-todo-list.js
    file and import function addList to index.js
    file.

--- src/data-todo-list.js
+++ src/data-todo-list.js
@@ -7,7 +7,42 @@ function list(title, description, dueDate, priority, notes, checklist){
     this.checklist = checklist
 }
 
-let myList = []
+function addList(
+    title,
+    description,
+    dueDate,
+    priority,
+    notes,
+    checklist){
+        myList.push(
+            new list(
+                title,
+                description,
+                dueDate,
+                priority,
+                notes,
+                checklist,
+            )
+        )
+    }
+
+
+// list.prototype.getIndex = function(){
+//     const num = myList.findIndex((el) => el === this)
+
+//     return num
+// }
+
+// list.prototype.delete = function(){
+//     let indexNum = this.getIndex()
+//     return myList.splice(indexNum, 1)
+// }
+
+// list.prototype.provideNumber = function(number){
+//     number++
+// }
+
+const myList = []
 
 export default list
-export {myList}
+export {myList, addList}
--- src/index.js
+++ src/index.js
@@ -1,47 +1,59 @@
 import './styles.css'
-import toDoListData, {myList} from './data-todo-list.js'
+import toDoListData, {myList, addList} from './data-todo-list.js'
 
-myList.push(
-    new toDoListData(
-        'play soccer',
-        'Go to private field with my friend',
-        'tomorrow',
-        'regular',
-        'bring billfold',
-        true,
-    ),
-    new toDoListData(
-        'repair my smartphone',
-        'visit my trustworthy enginner that can fix my smartphone',
-        'today',
-        'urgent',
-        'ask engineer for done repait process tomorrow',
-        false,
-    ),
-    new toDoListData(
-        'Go to Sixpath library',
-        'return expired rent Naruto light novel',
-        '9 February 2025',
-        'urgent',
-        'Ask for return book evidence from library staff',
-        false,
-    ),
-    new toDoListData(
-        'Go to traditional market',
-        'Buy vegetables and fruits',
-        'every morning',
-        'urgent',
-        '1 kg of banana, 1 kg of eggs, spinach',
-        true,
-    ),
-    new toDoListData(
-        'Clean my house',
-        'mop first and second floor',
-        'every Sunday',
-        'urgent',
-        'Check mop and broom',
-        true,
-    )
+
+addList(
+    'play soccer',
+    'Go to private field with my friend',
+    'tomorrow',
+    'regular',
+    'bring billfold and two pair of shoes',
+    true,
+)
+addList(
+    'repair my smartphone',
+    'visit my trustworthy enginner that can fix my smartphone',
+    'today',
+    'urgent',
+    'ask engineer for done repait process tomorrow',
+    false,
+)
+addList(
+    'Go to Sixpath library',
+    'return expired rent Naruto light novel',
+    '9 February 2025',
+    'urgent',
+    'Ask for return book evidence from library staff',
+    false,
+)
+addList(
+    'Go to traditional market',
+    'Buy vegetables and fruits',
+    'every morning',
+    'urgent',
+    '1 kg of banana, 1 kg of eggs, spinach',
+    true,
+)
+addList(
+    'Clean my house',
+    'mop first and second floor',
+    'every Sunday',
+    'urgent',
+    'Check mop and broom',
+    true,
 )
 
 console.log(myList)
+
+// function takeEl(arr, numStart, numMid){
+//     const arr1 = arr.slice(numStart, numMid)
+//     const arr2 = arr.slice(numMid + 1)
+//     const arrResult = arr1.concat(arr2)
+
+//     return arrResult
+// }
+// let test = myList.slice(0,2)
+// let test2 = myList.slice(3)
+// let testResult = test.concat(test2)
+
+// console.log([test, test2, testResult, takeEl(myList, 0, 2)])
