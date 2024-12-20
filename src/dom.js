f6934761aecaa9ae008fe7cc5cb5a488ea6d750e
Dewa Wono Ichsan <dewa.wono.ichsan@gmail.com>
2024-12-20 22:05:37 +0700

    Display list data from event click project navigation and event click submit list
    Add function delContent that contain system to
    remove element html. Function delContent have
    condition statement, when selector element of
    div with class name container-list is zero then
    return null. Everytime user submit data or change
    project to see todo list, delete previous
    displayed list and generate new display list
    based on variable myList using function
    displayList Alter.
    
    Use function delContent in event listener submit
    button at div with class with event click.
    
    Use function delContent in event listener project
    navigation.
    
    Refacture function displayListAlter because
    function get error when accept array with zero
    length in parameter arr. Add conditional
    statement when length of argument arr is zero
    then return null.

--- src/dom.js
+++ src/dom.js
@@ -13,7 +13,10 @@ const notes = document.querySelector('#list-notes')
 const checkList = document.querySelector('input#list-checklist')
 const submitList = document.querySelector('#add-list')
 
-submitList.addEventListener('click', function(){
+submitList.addEventListener('click', function(e){
+    
+    const arrData = myList.notInProject
+    
     addList(
         title.value,
         description.value,
@@ -23,7 +26,11 @@ submitList.addEventListener('click', function(){
         checkList.checked
     )
     console.log(myList)
-    event.preventDefault()
+
+    delContent()
+    displayListAlter(arrData)
+
+    e.preventDefault()
 })
 
 let count = 0
@@ -92,6 +99,8 @@ containerNotInProject.addEventListener('click',function(e){
     createSubmit.setAttribute('id', 'add-list')
     createSubmit.setAttribute('type', 'submit')
     createSubmit.textContent = 'submit'
+
+    const arrData = myList.notInProject
     
     createSubmit.addEventListener('click', function(e){
 
@@ -106,12 +115,17 @@ containerNotInProject.addEventListener('click',function(e){
 
         console.log(myList)
         
+        delContent()
+        displayListAlter(arrData)
 
         e.preventDefault()
     })
 
     const form = document.querySelector('.top > form')
     form.appendChild(createSubmit)
+
+    delContent()
+    displayListAlter(arrData)
 })
 
 const changeNavList2 = document.querySelector('.container-project')
@@ -126,6 +140,8 @@ changeNavList2.addEventListener('click', function(e){
     createSubmit.setAttribute('id', 'add-list')
     createSubmit.setAttribute('type', 'submit')
     createSubmit.textContent = 'submit'
+
+    const arrData = myList.project[`${selectedNav}`]
     
     createSubmit.addEventListener('click', function(e){
 
@@ -140,84 +156,106 @@ changeNavList2.addEventListener('click', function(e){
         )
 
         console.log(myList)
-        
+
+        delContent()
+        displayListAlter(arrData)
 
         e.preventDefault()
     })
 
     const form = document.querySelector('.top > form')
     form.appendChild(createSubmit)
+
+    delContent()
+    displayListAlter(arrData)
 })
 
 function displayListAlter(arr){
-    const content = document.querySelector('.content')
-    const divContainerList = document.createElement('div')
-    divContainerList.classList.toggle('container-list')
-    
-    content.appendChild(divContainerList)
 
-    const newContainerList = document.querySelector('.container-list')
-    
-    for (let index = 0; index < arr.length; index++) {
-        const divContainerTodoList = document.createElement('div')
-        divContainerTodoList.classList.toggle('container-todo-list')
+    if(arr.length <= 0){
 
-        newContainerList.appendChild(divContainerTodoList)
-    }
+        return null
+    } else{
 
-    for (let index = 0; index < arr.length; index++) {
-
-        const div = document.querySelector(`.container-todo-list:nth-child(${index + 1})`)
+        const content = document.querySelector('.content')
+        const divContainerList = document.createElement('div')
+        divContainerList.classList.toggle('container-list')
         
-        const divTitle = document.createElement('div')
-        divTitle.classList.toggle('title')
-        divTitle.textContent = `title: ${arr[index].title}`
-
-        div.appendChild(divTitle)
-
-        const divDescription = document.createElement('div')
-        divDescription.classList.toggle('description')
-        divDescription.textContent = `description: ${arr[index].description}`
-
-        div.appendChild(divDescription)
-
-        const divDueDate = document.createElement('div')
-        divDueDate.classList.toggle('dueDate')
-        divDueDate.textContent = `dueDate: ${arr[index].dueDate}`
-
-        div.appendChild(divDueDate)
-
-        const divPriority = document.createElement('div')
-        divPriority.classList.toggle('priority')
-        divPriority.textContent = `priority: ${arr[index].priority}`
-
-        div.appendChild(divPriority)
-
-        const divNotes = document.createElement('div')
-        divNotes.classList.toggle('notes')
-        divNotes.textContent = `notes: ${arr[index].notes}`
-
-        div.appendChild(divNotes)
-
-        const divChecklist = document.createElement('div')
-        divChecklist.classList.toggle('checklist')
-        divChecklist.textContent = `checklist: ${arr[index].checklist}`
-
-        div.appendChild(divChecklist)
-
-        const delBtn = document.createElement('button')
-        delBtn.classList.toggle('delete')
-        delBtn.textContent = 'delete'
-        delBtn.addEventListener('click', function(){
-            remove(arr, index)
-            console.log(myList)
-            divContainerList.remove()
-            displayListAlter(arr)
-        })
-
-        div.appendChild(delBtn)
+        content.appendChild(divContainerList)
+    
+        const newContainerList = document.querySelector('.container-list')
+        
+        for (let index = 0; index < arr.length; index++) {
+            const divContainerTodoList = document.createElement('div')
+            divContainerTodoList.classList.toggle('container-todo-list')
+    
+            newContainerList.appendChild(divContainerTodoList)
+        }
+    
+        for (let index = 0; index < arr.length; index++) {
+    
+            const div = document.querySelector(`.container-todo-list:nth-child(${index + 1})`)
+            
+            const divTitle = document.createElement('div')
+            divTitle.classList.toggle('title')
+            divTitle.textContent = `title: ${arr[index].title}`
+        
+            div.appendChild(divTitle)
+        
+            const divDescription = document.createElement('div')
+            divDescription.classList.toggle('description')
+            divDescription.textContent = `description: ${arr[index].description}`
+        
+            div.appendChild(divDescription)
+        
+            const divDueDate = document.createElement('div')
+            divDueDate.classList.toggle('dueDate')
+            divDueDate.textContent = `dueDate: ${arr[index].dueDate}`
+        
+            div.appendChild(divDueDate)
+        
+            const divPriority = document.createElement('div')
+            divPriority.classList.toggle('priority')
+            divPriority.textContent = `priority: ${arr[index].priority}`
+        
+            div.appendChild(divPriority)
+        
+            const divNotes = document.createElement('div')
+            divNotes.classList.toggle('notes')
+            divNotes.textContent = `notes: ${arr[index].notes}`
+        
+            div.appendChild(divNotes)
+        
+            const divChecklist = document.createElement('div')
+            divChecklist.classList.toggle('checklist')
+            divChecklist.textContent = `checklist: ${arr[index].checklist}`
+        
+            div.appendChild(divChecklist)
+        
+            const delBtn = document.createElement('button')
+            delBtn.classList.toggle('delete')
+            delBtn.textContent = 'delete'
+            delBtn.addEventListener('click', function(){
+                remove(arr, index)
+                console.log(myList)
+                divContainerList.remove()
+                displayListAlter(arr)
+            })
+        
+            div.appendChild(delBtn)
+        
+        }
 
     }
+}
+
+function delContent(){
+
+    if(document.querySelector('.container-list') === null){
+        return null
+    }
+    const divContainerList = document.querySelector('.container-list')
+    divContainerList.remove()
 }
 
 export{
