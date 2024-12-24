4d71da643b6717809cb86f1b83a852348c6eb376
Dewa Wono Ichsan <dewa.wono.ichsan@gmail.com>
2024-12-24 23:19:37 +0700

    Refacture function getTodoListData
    Add variable setNotInProject for contain array
    from data.notInProject. Then use map array method
    for change data from ordinary object to object
    created from function object constructor list.
    
    Add function getProject for change object in
    project variable data. Add variable setProject
    contain empty object in scope function
    getProject. Add variable arr for contain
    data.project key. Use loop method for change
    data.project from ordinary object to object
    constructed from function object constructor
    list. input values from loop to variable
    setProject then return variable setProject.
    
    Set value of myList.notInProject with variable
    setNotInProject and set value of myList.project
    with return value of function getProject.

--- src/data-todo-list.js
+++ src/data-todo-list.js
@@ -9,6 +9,10 @@ function list(title, description, dueDate, priority, notes, checklist){
     this.checklist = checklist
 }
 
+list.prototype.getTitle = function(){
+    return this.title
+}
+
 function addList(
     title,
     description,
@@ -65,15 +69,53 @@ const myList = {
 }
 
 function setTodoListData(){
+
     localStorage.setItem('local save', JSON.stringify(myList))
+
 }
+
 function getTodoListData(){
+
     const data = JSON.parse(localStorage.getItem('local save'))
+
     if(data === null){
         return null
     }
-    myList.notInProject = data.notInProject
-    myList.project = data.project
+
+    const setNotInProject = data.notInProject.map(
+        value => new list(
+            value.title,
+            value.description,
+            value.dueDate,
+            value.priority,
+            value.notes,
+            value.checklist
+        )
+    )
+    
+    function getProject(){
+        const setProject = {}
+        const arr = Object.keys(data.project)
+
+        for (let index = 0; index < arr.length; index++) {
+            setProject[`${arr.at(index)}`] = data.project[`${arr.at(index)}`]
+            .map(value => new list(
+                value.title,
+                value.description,
+                value.dueDate,
+                value.priority,
+                value.notes,
+                value.checklist
+                )
+            )
+        }
+
+        return setProject
+
+    }
+
+    myList.notInProject = setNotInProject
+    myList.project = getProject()
 }
 
 export default list
--- src/index.js
+++ src/index.js
@@ -4,46 +4,46 @@ import remove, {delList} from './remove-todo-list.js'
 import {addProjectBtn, valueProject, containerList, displayList, displayListAlter, displayProject} from './dom.js'
 
 
-addList(
-    'play soccer',
-    'Go to private field with my friend',
-    'tomorrow',
-    'regular',
-    'bring billfold and two pair of shoes',
-    true,
-)
-addList(
-    'repair my smartphone',
-    'visit my trustworthy enginner that can fix my smartphone',
-    'today',
-    'urgent',
-    'ask engineer for done repait process tomorrow',
-    false,
-)
-addList(
-    'Go to Sixpath library',
-    'return expired rent Naruto light novel',
-    '9 February 2025',
-    'urgent',
-    'Ask for return book evidence from library staff',
-    false,
-)
-addList(
-    'Go to traditional market',
-    'Buy vegetables and fruits',
-    'every morning',
-    'urgent',
-    '1 kg of banana, 1 kg of eggs, spinach',
-    true,
-)
-addList(
-    'Clean my house',
-    'mop first and second floor',
-    'every Sunday',
-    'urgent',
-    'Check mop and broom',
-    true,
-)
+// addList(
+//     'play soccer',
+//     'Go to private field with my friend',
+//     'tomorrow',
+//     'regular',
+//     'bring billfold and two pair of shoes',
+//     true,
+// )
+// addList(
+//     'repair my smartphone',
+//     'visit my trustworthy enginner that can fix my smartphone',
+//     'today',
+//     'urgent',
+//     'ask engineer for done repait process tomorrow',
+//     false,
+// )
+// addList(
+//     'Go to Sixpath library',
+//     'return expired rent Naruto light novel',
+//     '9 February 2025',
+//     'urgent',
+//     'Ask for return book evidence from library staff',
+//     false,
+// )
+// addList(
+//     'Go to traditional market',
+//     'Buy vegetables and fruits',
+//     'every morning',
+//     'urgent',
+//     '1 kg of banana, 1 kg of eggs, spinach',
+//     true,
+// )
+// addList(
+//     'Clean my house',
+//     'mop first and second floor',
+//     'every Sunday',
+//     'urgent',
+//     'Check mop and broom',
+//     true,
+// )
 // localStorage.clear()
 getTodoListData()
 // addProject()
